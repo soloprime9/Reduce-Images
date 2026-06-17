4ab834621eefeda0f469f7e7c0cbdc3621053dc1
soloprime9 <primeindia5750@gmail.com>
2026-06-17 17:22:44 +0530

    Update page.jsx

--- src/app/zipconvert/page.jsx
+++ src/app/zipconvert/page.jsx
@@ -1,13 +1,13 @@
 // src/app/zipconvet/page.jsx
 import React from 'react';
-import ZipExporter from '@/components/ZipExporter'; // '@/' points to your 'src/' directory
+// import ZipExporter from '@/components/ZipExporter'; // '@/' points to your 'src/' directory
 
 export default function ZipConvertPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-2xl font-bold mb-4">Zip File Converter</h1>
       {/* Rendering your custom component here */}
-      <ZipExporter />
+      {/* <ZipExporter /> */}
     </main>
   );
 }
